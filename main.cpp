fc8f6d1cc040f3822e1d4a2b7e4b035b09559dc4
Rustam <114682879+Rust163@users.noreply.github.com>
2023-02-13 19:30:36 +0000

    Array

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+    srand(time(0));
+    int size_1 = 15;
+    int size_2 = 15;
+    int arr1[size_1] {
+        0
+    };
+    int arr2[size_2] {
+        0
+    };
+    for(int i = 0; i <= size_1; i++) {
+        arr1[i] = rand() % 100;
+        cout << "arr1[" << i << "] " << arr1[i] << endl;
+    }
+    cout << endl;
+    for(int j = 0; j < size_2; j++) {
+        arr2[j] = rand() % 100;
+        cout << "arr2[" << j << "] " << arr2[j] << endl;
+    }
 }
